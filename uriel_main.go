a18e142d123e1a97f356db259e142f465e32686e
Troy <25730043+Maurite@users.noreply.github.com>
2017-09-15 10:26:02 -0400

    Functional changes
    - Replaced `fmt` library for `log` lib
    - Command structure was re-newed. Changed to Go maps for easier value access

--- uriel_main.go
+++ uriel_main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"strings"
 	"log"
-	"fmt"
 	"time"
 	"io/ioutil"
 	"github.com/bwmarrin/discordgo"
@@ -52,7 +51,7 @@ func main() {
 	check(err)
 	
 	conf.BotID = u.ID
-	conf.Session.AddHandler(commands)
+	conf.Session.AddHandler(ChatMonitor)
 
 	err = conf.Session.Open()
 	check(err)
@@ -65,64 +64,86 @@ func main() {
 	return
 }
 
-func commands(s *discordgo.Session, m *discordgo.MessageCreate) {
+// Commands
+func cmdHelp(args []string) {
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"Comandos Uriel:\n**!hora** : Muestra la hora de España y del host\n**!noticias** : Muestra las noticias mas recientes de la página de Metin2.es\n**!eventos** : Muestra los eventos fijos del juego")
+}
+
+func cmdHour(args []string) {
+	t := CEST()
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"Hora actual Venezuela : " + time.Now().Format(time.RFC850) + "\nHora actual España : " + t.Full)
+}
+
+func cmdNews(args []string) {
+	resp, err := http.Get("https://board.es.metin2.gameforge.com/")
+	check(err)
+
+	root, err := html.Parse(resp.Body)
+	check(err)
+
+	matcher := func(n *html.Node) bool {
+		if n.DataAtom == atom.Header || n.DataAtom == atom.Div {
+			return scrape.Attr(n,"class") == "messageHeader" || scrape.Attr(n,"class") == "messageText"
+		}
+		return false
+	}
+
+	articles := scrape.FindAll(root, matcher)
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**Noticias Metin2.es**")
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[0]) + "**" + "\n" + scrape.Text(articles[1]))
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[2]) + "**" + "\n" + scrape.Text(articles[3]))
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[4]) + "**" + "\n" + scrape.Text(articles[5]))
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[6]) + "**" + "\n" + scrape.Text(articles[7]))
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[8]) + "**" + "\n" + scrape.Text(articles[9]))
+}
+
+func cmdEvents(args []string) {
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"**Eventos Fijos Metin2.es**")
+	_, _ = conf.Session.ChannelMessageSend(conf.Channel,"\n**Lunes:**\n\t22:00-00:00 Drop de supermonturas\n\n**Martes:**\n\t16:00-18:00 Drop de supermonturas\n\n**Miércoles:**\n\t23:00-01:00 Drop de supermonturas\n\n**Jueves:**\n\t13:00-15:00 Drop de supermonturas\n\n**Viernes:**\n\t18:00-20:00 Drop de supermonturas\n\n**Sábado:**\n\t13:00-15:00 Drop de supermonturas\n\t18:00-00:00 (El segundo sábado de cada mes) Drop de cajas luz luna\n\tTodo el día (Último fin de semana de cada mes) Festival de la cosecha\n\n**Domingo:**\n\t13:00-15:00 Drop de supermonturas\n\t22:00-02:00 Drop de Alubias verdes del dragón\n\tTodo el día (Último fin de semana de cada mes) Festival de la cosecha\n\n**Cualquier día de la semana:**\n\tCompetición OX (Mínimo una vez por semana)\n\tDrop de telas delicadas 4 h. (Sin día ni hora predefinido, sale el mensaje en el juego cuando comienza)\n\tDrop de Cor Draconis 4 h. (Sin día ni hora predefinido, sale el mensaje en el juego cuando comienza).")
+}
+//------------
+
+func LoadCommands() map[string]func([]string) {
+	return map[string]func([]string){
+		"!ayuda":cmdHelp,
+		"!hora":cmdHour,
+		"!noticias":cmdNews,
+		"!eventos":cmdEvents,
+	}
+}
+
+func ChatMonitor(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == conf.BotID {
 		return
 	}
-	
-	var cmd, params, params2, params3 string
-	fmt.Sscanf(m.Content,"%s %s %s %s",&cmd,&params,&params2,&params3)
 
-	if cmd == "!cmds" || cmd == "!cmd" || cmd == "!comandos" || cmd == "!commands" {
-		_, _ = s.ChannelMessageSend(conf.Channel,"Comandos Uriel:\n!hora : Muestra la hora local de Uriel\n!horaesp : Muestra la hora de España\n!noticias : Muestra las noticias mas recientes de la página de Metin2.es\n!eventos : Muestra los eventos fijos del juego")
+	if !strings.HasPrefix(m.Content, "!") {
+		return
 	}
 
-	if cmd == "!hora" {
-		_, _ = s.ChannelMessageSend(conf.Channel,"Hora actual Venezuela : " + time.Now().Format(time.RFC850))
+	var cmd map[string]func([]string)
+	cmd = make(map[string]func([]string))
+	cmd = LoadCommands()
+
+	input := strings.Split(m.Content," ")
+
+	command := strings.ToLower(input[0])
+
+	val, exist := cmd[command]
+	if exist {
+		val(input)
+	} else {
+		_, _ = conf.Session.ChannelMessageSend(conf.Channel,"Comando inválido. Usa **!ayuda** para ver la lista de comandos disponibles")
 	}
 
-	if cmd == "!horaesp" {
-		t := CEST()
-
-		_, _ = s.ChannelMessageSend(conf.Channel,"Hora actual España : " + t.Full)
-	}
-
-	if cmd == "!noticias" || cmd == "!news" {
-		resp, err := http.Get("https://board.es.metin2.gameforge.com/")
-		check(err)
-
-		root, err := html.Parse(resp.Body)
-		check(err)
-
-		matcher := func(n *html.Node) bool {
-			if n.DataAtom == atom.Header || n.DataAtom == atom.Div {
-				return scrape.Attr(n,"class") == "messageHeader" || scrape.Attr(n,"class") == "messageText"
-			}
-			return false
-		}
-
-		articles := scrape.FindAll(root, matcher)
-		_, _ = s.ChannelMessageSend(conf.Channel,"**Noticias Metin2.es**")
-		_, _ = s.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[0]) + "**" + "\n" + scrape.Text(articles[1]))
-		_, _ = s.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[2]) + "**" + "\n" + scrape.Text(articles[3]))
-		_, _ = s.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[4]) + "**" + "\n" + scrape.Text(articles[5]))
-		_, _ = s.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[6]) + "**" + "\n" + scrape.Text(articles[7]))
-		_, _ = s.ChannelMessageSend(conf.Channel,"**" + scrape.Text(articles[8]) + "**" + "\n" + scrape.Text(articles[9]))
-	}
-
-
-	if cmd == "!eventos" || cmd == "!event" || cmd == "!events" || cmd == "!evento" {
-		_, _ = s.ChannelMessageSend(conf.Channel,"**Eventos Metin2.es**")
-		_, _ = s.ChannelMessageSend(conf.Channel,"\n**Lunes:**\n\t22:00-00:00 Drop de supermonturas\n\n**Martes:**\n\t16:00-18:00 Drop de supermonturas\n\n**Miércoles:**\n\t23:00-01:00 Drop de supermonturas\n\n**Jueves:**\n\t13:00-15:00 Drop de supermonturas\n\n**Viernes:**\n\t18:00-20:00 Drop de supermonturas\n\n**Sábado:**\n\t13:00-15:00 Drop de supermonturas\n\t18:00-00:00 (El segundo sábado de cada mes) Drop de cajas luz luna\n\tTodo el día (Último fin de semana de cada mes) Festival de la cosecha\n\n**Domingo:**\n\t13:00-15:00 Drop de supermonturas\n\t22:00-02:00 Drop de Alubias verdes del dragón\n\tTodo el día (Último fin de semana de cada mes) Festival de la cosecha\n\n**Cualquier día de la semana:**\n\tCompetición OX (Mínimo una vez por semana)\n\tDrop de telas delicadas 4 h. (Sin día ni hora predefinido, sale el mensaje en el juego cuando comienza)\n\tDrop de Cor Draconis 4 h. (Sin día ni hora predefinido, sale el mensaje en el juego cuando comienza).")
-	}
 
 }
 
 type TimeEx struct {
 	Hour		int
-	Min			int
-	Sec			int
-	Day			int
+	Min		int
+	Sec		int
+	Day		int
 	Weekday		string
 	Month		string
 	Full	 	string
